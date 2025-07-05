782e4f4a26ddc127f0e50383f572856479c71a12
Chintan Patel <chintanpatelit024@gmail.com>
2025-07-05 19:32:46 +0530

    Added: Make VFX Pool

--- Assets/Scripts/GameService.cs
+++ Assets/Scripts/GameService.cs
@@ -11,7 +11,6 @@ using CosmicCuration.Utilities;
 using CosmicCuration.PowerUps; 
 #endregion
 
-
 public class GameService : GenericMonoSingleton<GameService>
 {
     #region Dependencies
@@ -29,6 +28,7 @@ public class GameService : GenericMonoSingleton<GameService>
     [SerializeField] private PlayerView playerPrefab;
     [SerializeField] private BulletView playerBulletPrefab;
     [SerializeField] private EnemyView enemyPrefab;
+    [SerializeField] private VFXView vfxPrefab;
     #endregion
 
     #region Scriptable Objects
@@ -37,7 +37,6 @@ public class GameService : GenericMonoSingleton<GameService>
     [SerializeField] private EnemyScriptableObject enemyScriptableObject;
     [SerializeField] private PowerUpScriptableObject powerUpScriptableObject;
     [SerializeField] private SoundScriptableObject soundScriptableObject;
-    [SerializeField] private VFXScriptableObject vfxScriptableObject;
     #endregion
 
     #region Scene References
@@ -52,7 +51,7 @@ public class GameService : GenericMonoSingleton<GameService>
         playerService = new PlayerService(playerPrefab, playerScriptableObject, playerBulletPrefab, playerBulletScriptableObject);
         powerUpService = new PowerUpService(powerUpScriptableObject);
         enemyService = new EnemyService(enemyPrefab, enemyScriptableObject);
-        vfxService = new VFXService(vfxScriptableObject);
+        vfxService = new VFXService(vfxPrefab);
     }
 
     private void Update()
@@ -74,5 +73,4 @@ public class GameService : GenericMonoSingleton<GameService>
 
     public UIView GetUIService() => uiService; 
     #endregion
-
 }
